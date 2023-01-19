aabb5a0df2f2e6b0d55d8a26006cabeafd31d260
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-01-19 23:14:33 +0600

    Added footer part

--- src/Layouts/Main.js
+++ src/Layouts/Main.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import './Main.css';
-import { Outlet, useLoaderData } from 'react-router-dom';
-import Header from '../Components/Header/Header';
-import Quizs from '../Components/Quizs/Quizs';
+import React from "react";
+import "./Main.css";
+import { Outlet, useLoaderData } from "react-router-dom";
+import Header from "../Components/Header/Header";
+import Quizs from "../Components/Quizs/Quizs";
+import Footer from "../Components/footer/Footer";
 
 const Main = () => {
-    const quizs = useLoaderData();
-    return (
-        <div >
-            <Header></Header>
-            <Outlet></Outlet>
-
-
-        </div>
-    );
+  const quizs = useLoaderData();
+  return (
+    <div>
+      <Header></Header>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Main;
